b9a4b2070e3f49810e5a2af9df04893fb27e0a17
Byron Gibson <fbg111@gmail.com>
2011-10-02 21:18:21 -0700

    update:ToDo.hs

--- ToDo.hs
+++ ToDo.hs
@@ -14,27 +14,37 @@ dispatch :: String -> [String] -> IO ()
 dispatch "add"      = add
 dispatch "view"     = view
 dispatch "remove"   = remove
+-- dispatch "bump"  = bump
+dispatch command    = errDNE command
+--dispatch            = errNoArgs
 
+-- Handle no arguments
+-- Q: How do you represent no args in dispatch function above?  Tried "", [], _, blank space... all result in type error
+errNoArgs :: IO ()
+errNoArgs = putStrLn "You must enter a command: view | add | remove."
+
+-- Handle malformed command argument
+errDNE :: String -> [String] -> IO ()
+errDNE command _ = putStrLn $ "The command '" ++ command ++ "' does not exist."
+
+-- Add new todo item, one at a time
 add :: [String] -> IO ()
-add [] = putStrLn "error [] :: Please specify the file to add to, and the item to add."
-add [s] = putStrLn "error [s] :: I need two arguments: file to add to, and item to add."
-add [f,i,_] = putStrLn "error [f,i,_] :: Please, only one item at a time: file to add to, followed by one item to add."
 add [fileName,todoItem] = appendFile fileName (todoItem ++ "\n")
+add _ = putStrLn "error :: I need exactly two arguments: the file to add to, and the item to add."
 
+-- View all todo items
 view :: [String] -> IO ()
-view [] = putStrLn "error [] :: Please specify a file to view."
+--view [] = putStrLn "error [] :: Please specify a file to view."
 view [fileName] = do
     contents <- readFile fileName
     let todoTasks = lines contents
         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) 
                         [0..] todoTasks
     mapM_ putStrLn numberedTasks
-    -- putStrLn unlines $ numberedTasks
+view _ = putStrLn "error :: I need exactly one argument: the file to view."
 
+-- Delete a todo item and show the new todo list
 remove :: [String] -> IO ()
-remove [] = putStrLn "error [] :: I need two arguments: file to delete from, and item to delete."
-remove [s] = putStrLn "error [s] :: I need two arguments: file to delete from, and item to delete."
-remove [f,i,_] = putStrLn "error [f,i,_] :: Please, only one item at a time: file to remove from, followed by one item to remove."
 remove [fileName, numberString] = do
     contents <- readFile fileName
     let todoTasks = lines contents
@@ -52,5 +62,9 @@ remove [fileName, numberString] = do
             hClose tempHandle
             removeFile "todo.txt"
             renameFile tempName "todo.txt")
+remove _ = putStrLn "error :: I need exactly two arguments: the file to delete from, and the number of the item to delete."
 
-
+-- Move the specified todo item to the top of the list
+-- bump :: [String] -> IO ()
+-- bump [fileName, numberString] = do ...
+-- bump _ = putStrLn "error :: I need exactly two arguments: the todo list filename, and the number of the item to bump to the top."
